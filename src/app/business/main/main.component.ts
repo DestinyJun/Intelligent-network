eb3a2492cc03497226b317caeeb176222125e2ce
DestinyJun <429771890@qq.com>
2018-09-16 23:50:48 +0800

    常规修改： 1.增加底部事件统计

--- src/app/business/main/main.component.ts
+++ src/app/business/main/main.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {NgxEchartsService} from 'ngx-echarts';
 import {animation} from './right-sider-animation';
